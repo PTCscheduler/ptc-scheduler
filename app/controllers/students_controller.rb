718ebd88b921548ffa33c3f9ff2c53dc1af367e5
Ciprianna <ciprianna17@gmail.com>
2015-11-11 22:15:01 -0600

    Created RESTful controller methods to match routes; NO views yet!

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,49 @@
 class StudentsController < ApplicationController
+
+  def index
+    @students = Student.all
+  end
+
+  def show
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def edit
+  end
+
+  def create
+    @student = Student.new(student_params)
+
+    if @student.save
+        redirect_to @student
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @student.update(student_params)
+        redirect_to @student
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @student.destroy
+    render :index
+  end
+
+  private
+  def set_student
+    @student = Student.find(params[:id])
+  end
+
+  def student_params
+    params.require(:student).permit(:first_name, :last_name)
+  end
+
 end
