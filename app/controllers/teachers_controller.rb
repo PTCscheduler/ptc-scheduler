d568f8fd5d69888528ce4a4031793a3fa7e0e378
Ciprianna <ciprianna17@gmail.com>
2015-11-14 13:54:05 -0600

    Generated teacher controller. Added controller methods.

---
+++ app/controllers/teachers_controller.rb
@@ -0,0 +1,57 @@
+class TeachersController < ApplicationController
+  before_action :set_teachers, only: [:show, :edit, :update, :destroy]
+
+  # GET /teachers
+  def index
+    @teachers = Teacher.all
+  end
+
+  # GET /teachers/1
+  def show
+  end
+
+  # GET /teachers/new
+  def new
+    @teacher = Teacher.new
+  end
+
+  # GET /teachers/1/edit
+  def edit
+  end
+
+  # POST /teachers
+  def create
+    @teacher = Teacher.new(teacher_params)
+
+    if @teacher.save
+        redirect_to @teacher, notice: 'Teacher was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /teachers/1
+  def update
+    if @teacher.update(teacher_params)
+        redirect_to @teacher, notice: 'Teacher information was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /teachers/1
+  def destroy
+    @teacher.destroy
+    render :index, notice: 'Teacher was successfully deleted.'
+  end
+
+  private
+  def set_teacher
+    @teacher = Teacher.find(params[:id])
+  end
+
+  def teacher_params
+    params.require(:teacher).permit(:first_name, :last_name, :email, :password)
+  end
+
+end
