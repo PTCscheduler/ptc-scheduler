d2c8d3f32960dd1cf29b86b4c8684acf485d46b6
Ciprianna <ciprianna17@gmail.com>
2015-11-14 11:26:08 -0600

    Added documentation to Students controller; Created a very basic   student index view.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,40 +1,47 @@
 class StudentsController < ApplicationController
 
+  # GET /students
   def index
     @students = Student.all
   end
 
+  # GET /students/1
   def show
   end
 
+  # GET /students/new
   def new
     @student = Student.new
   end
 
+  # GET /students/1/edit
   def edit
   end
 
+  # POST /students
   def create
     @student = Student.new(student_params)
 
     if @student.save
-        redirect_to @student
+        redirect_to @student, notice: 'Student was successfully created.'
     else
       render :new
     end
   end
 
+  # PATCH/PUT /students/1
   def update
     if @student.update(student_params)
-        redirect_to @student
+        redirect_to @student, notice: 'Student information was successfully updated.'
     else
       render :edit
     end
   end
 
+  # DELETE /students/1
   def destroy
     @student.destroy
-    render :index
+    render :index, notice: 'Student was successfully deleted.'
   end
 
   private
