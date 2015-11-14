6f049a4f9029915e21ef413abe77d4d1c48132a7
Ciprianna <ciprianna17@gmail.com>
2015-11-14 13:00:45 -0600

    Fixed student-teacher relationship.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,5 @@
 class Student < ActiveRecord::Base
-  belongs_to :teachers
+  has_many_and_belongs_to :teachers
   validates :first_name, presence: true
   validates :last_name, presence: true
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,5 +1,5 @@
 class Teacher < ActiveRecord::Base
-  has_many :students
+  has_many_and_belongs_to :students
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
