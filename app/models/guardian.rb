f868019cf1bf7becf287693823698b83bed8d154
Cameron Helkuik <cameronhelkuik@gmail.com>
2015-11-30 10:32:55 -0600

    Added has and belongs to guardians and students

--- app/models/guardian.rb
+++ app/models/guardian.rb
@@ -1,2 +1,5 @@
 class Guardian < ActiveRecord::Base
+  has_and_belongs_to_many :students
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,6 @@
 class Student < ActiveRecord::Base
   has_and_belongs_to_many :teachers
+  has_and_belongs_to_many :guardians
   validates :first_name, presence: true
   validates :last_name, presence: true
 end
