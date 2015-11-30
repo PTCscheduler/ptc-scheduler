714c653edc7b2871b7019a147e2470aceacf02d6
Cameron Helkuik <cameronhelkuik@gmail.com>
2015-11-30 10:43:21 -0600

    updated controller

--- app/controllers/guardians_controller.rb
+++ app/controllers/guardians_controller.rb
@@ -1,4 +1,52 @@
-class GuardiansController < InheritedResources::Base
+class GuardiansController < ApplicationController
+
+  # GET /guardians
+  def index
+    @guardian = Guardian.all
+  end
+
+  # GET /guardians/1
+  def show
+  end
+
+  # GET /guardians/new
+  def new
+    @guardian = Guardian.new
+  end
+
+  # GET /guardians/1/edit
+  def edit
+  end
+
+  # POST /guardians
+  def create
+    @guardian = Guardian.new(guardian_params)
+
+    if @guardian.save
+        student = Student.find(params["student"]["guardian"])
+        @guardian.student << student
+        redirect_to @guardian, notice: 'Guardian was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /guardians/1
+  def update
+    if @guardian.update(guardian_params)
+      student = Student.find(params["student"]["guardian_id"])
+      @guardian.student << student
+        redirect_to @guardian, notice: 'Guardian information was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /guardians/1
+  def destroy
+    @guardian.destroy
+    redirect_to @guardians, notice: 'Student was successfully deleted.'
+  end
 
   private
 
