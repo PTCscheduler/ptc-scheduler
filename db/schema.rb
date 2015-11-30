936c01673eb8dec5015f1cec9d0f21b2ccaeca41
Cameron Helkuik <cameronhelkuik@gmail.com>
2015-11-30 11:11:33 -0600

    created guardian_student join table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151130162726) do
+ActiveRecord::Schema.define(version: 20151130170921) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -52,6 +52,11 @@ ActiveRecord::Schema.define(version: 20151130162726) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "guardians_students", id: false, force: :cascade do |t|
+    t.integer "guardian_id", null: false
+    t.integer "student_id", null: false
   end
 
   create_table "students", force: :cascade do |t|
